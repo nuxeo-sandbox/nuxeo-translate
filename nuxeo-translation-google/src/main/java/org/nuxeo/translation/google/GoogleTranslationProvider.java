4b68f9f2cb6fb9b1d8378885d6a4ccee27836fce
Jackie Aldama <jaldama@localhost.localdomain>
2017-09-08 11:22:32 -0400

    Fix typo

--- nuxeo-translation-google/src/main/java/org/nuxeo/translation/google/GoogleTranslationProvider.java
+++ nuxeo-translation-google/src/main/java/org/nuxeo/translation/google/GoogleTranslationProvider.java
@@ -88,7 +88,7 @@ public class GoogleTranslationProvider implements TranslationProvider {
 
 		Translate translationService = getTranslationServiceClient();
 
-		//Translation response = translationService.translate(text);
+		Translation response = translationService.translate(text);
 		//response = translationService.translate(text, TranslateOption.sourceLanguage(sourceLanguage), TranslateOption.targetLanguage(targetLanguage));
 		//Translation response = translationService.translate(text, Translate.TranslateOption.sourceLanguage("es"));
 		//System.out.println(response.getTranslatedText());
